d6eaf30c8f93a7261370d105110997af1b6959b4
nguyenngoctruongson <nguyenngoctruongson06102003@gmail.com>
2023-11-24 14:36:34 +0700

    son

---
+++ duan1/view/cart/viewcart.php
@@ -0,0 +1,14 @@
+<div class="giohang1">
+<div class="giohang2">Giỏ Hàng</div>
+<div class="giohang">
+    <table >
+        <?php
+        viewcart(1);
+        ?>
+    </table>
+    <div style="background-color: #f2f2f2">
+        <a href="index.php?act=bill">    <input type="submit" value="dong y dat hang"></a>
+        <a href="index.php?act=delcart">    <input type="submit" value="xoa gio hang"></a>
+    </div>
+</div>
+</div>
